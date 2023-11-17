9aa68bd442ff6fcb555c05662f8b042ba56e7700
Vikuzzz <146830867+Vikuzzz@users.noreply.github.com>
2023-11-17 10:53:59 +0000

    modifiche2

--- esercizi/ristorante/index.php
+++ esercizi/ristorante/index.php
@@ -23,12 +23,13 @@
     </style>
     
         <?php
-        $a = [" Pasta al pesto ", " Pasta alla carbonara ", " Gnocchi ai 4 formaggi ", " Ravioli alla zucca "];
-        mettere un altro array con testo e prezzo
-        foreach($a as $piatto)
+        $a = [" Pasta al pesto " => 10, " Pasta alla carbonara " => 10, " Gnocchi ai 4 formaggi " => 10, " Ravioli alla zucca " => 10];
+        
+        //mettere un altro array con testo e prezzo
+        foreach($a as $piatto => $prezzo)
         {     
-            echo "<li>$piatto</li>";
-            echo "Il pesto alla genovese è un condimento tradizionale tipico originario della Liguria. Con tale denominazione è inserito tra i Prodotti agroalimentari tradizionali liguri. Il suo ingrediente base è il basilico, e più specificamente il Basilico Genovese. ";
+            echo "<li>$piatto $prezzo</li>";
+            //echo "Il pesto alla genovese è un condimento tradizionale tipico originario della Liguria. Con tale denominazione è inserito tra i Prodotti agroalimentari tradizionali liguri. Il suo ingrediente base è il basilico, e più specificamente il Basilico Genovese. ";
         }
         ?>
     </body>
