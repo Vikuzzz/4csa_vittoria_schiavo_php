a41165274da52f8f8f81c1cdb9afd8ef4e7cf6eb
Vikuzzz <146830867+Vikuzzz@users.noreply.github.com>
2023-11-17 14:06:57 +0000

    modifiche4

--- esercizi/ristorante/index.php
+++ esercizi/ristorante/index.php
@@ -3,32 +3,45 @@
         <title>Sito dinamico di un ristorante ricorrendo a php</title>
     </head>
 
-    <body>
+    <body bgcolor="#EEE8AA">
 
+    <h1> RISTORANTE DA VITTORIA </h1>
+    <h2> Primi Piatti </h2>
+    
     <style>
 
-    #Pastaalpesto
-    {
-        position: relative;
-        left: 0;
-        top:0;
-        width:200px;
-        height:30px;
-        margin-top:20px;
-        margin-left:auto;
-    }
+        h1
+        {
+            
+        }
     </style>
     
         <?php
         $a = [" Pasta al pesto " => 10, " Pasta alla carbonara " => 10, " Gnocchi ai 4 formaggi " => 10, " Ravioli alla zucca " => 10];
-        
-        //mettere un altro array con testo e prezzo
+
         foreach($a as $piatto => $prezzo)
         {     
             echo "<li>$piatto $prezzo</li>";
-            //echo "Il pesto alla genovese è un condimento tradizionale tipico originario della Liguria. Con tale denominazione è inserito tra i Prodotti agroalimentari tradizionali liguri. Il suo ingrediente base è il basilico, e più specificamente il Basilico Genovese. ";
+
+            if($piatto == " Pasta al pesto ")
+            {
+                echo " Ingredienti: Spaghetti, basilico, pinoli, olio, parmiggiano reggiano ";
+            }
+            else if($piatto == " Pasta alla carbonara ")
+            {
+               echo " Ingredienti: Spaghetti, uova, pecorino, guanciale ";
+            }
+            else if($piatto == " Gnocchi ai 4 formaggi ")
+            {
+               echo " Ingredienti: Gnocchi, gorgonzola, pecorino, parmiggiano reggiano ";
+            }
+            else if($piatto == " Ravioli alla zucca ")
+            {
+               echo " Ingredienti: Ravioli, zucca, grana padano ";
+            }
         }
         ?>
+
     </body>
 </html>
 
