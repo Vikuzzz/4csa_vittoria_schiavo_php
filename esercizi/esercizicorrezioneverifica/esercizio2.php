82f19b4ae5d62b5a6919bc88fd91df973b98ea71
Vikuzzz <146830867+Vikuzzz@users.noreply.github.com>
2023-12-27 11:47:12 +0000

    modifiche4

--- esercizi/esercizicorrezioneverifica/esercizio2.php
+++ esercizi/esercizicorrezioneverifica/esercizio2.php
@@ -20,7 +20,7 @@
         text-align: center;
     }
 
-    </style>
+</style>
 
 <h1 style="color:#A52A2A; text-align: center; font-family: Times New Roman', Times, serif;"><b> BENVENUT* NEL FORM CHE HAI COMPILATO, QUESTE SONO LE TUE INFORMAZIONI. </b></h1>
 
@@ -29,32 +29,43 @@
     $passwordgiusta = "info2023";
     $utentegiusto = "utente";
     $contatoretentativi = 0;
+    $tentativimassimi = 3;
 
-    if($_GET['password'] == $passwordgiusta && $_GET['nome'] ==  $utentegiusto && $contatoretentativi <= 3)
+    if($_GET['password'] == $passwordgiusta && $_GET['nome'] ==  $utentegiusto && $contatoretentativi <= $tentativimassimi)
     {
 
         echo "<h1 style='color:black; text-align:center;'> Hai eseguito l'accesso correttamente! </h1>";
 
-    }else if($_GET['password'] == $passwordgiusta && $_GET['nome'] !=  $utentegiusto && $contatoretentativi <= 3)
+    }else if($_GET['password'] == $passwordgiusta && $_GET['nome'] !=  $utentegiusto && $contatoretentativi <= $tentativimassimi)
     {
 
         echo "<h1 style='color:black; text-align:center;'> Nome utente sbagliato! Riprova a rienserire i tuoi dati fino allo scadere dei tentativi. </h1>";
+        echo "<a href=\"esercizio2.html\">Torna al form</a>";
+        $contatoretentativi++;
 
-    }else if($_GET['password'] != $passwordgiusta && $_GET['nome'] ==  $utentegiusto && $contatoretentativi <= 3)
+    }else if($_GET['password'] != $passwordgiusta && $_GET['nome'] ==  $utentegiusto && $contatoretentativi <= $tentativimassimi)
     {
 
         echo "<h1 style='color:black; text-align:center;'> Passoword errata. Riprova a rienserire i tuoi dati fino allo scadere dei tentativi. </h1>";
+        echo "<a href=\"esercizio2.html\">Torna al form</a>";
+        $contatoretentativi++;
 
-    }else
+    }else if($_GET['password'] != $passwordgiusta && $_GET['nome'] !=  $utentegiusto && $contatoretentativi <= $tentativimassimi)
+    {
+
+        echo "<h1 style='color:black; text-align:center;'> Passoword e nome utente errati. Riprova a rienserire i tuoi dati fino allo scadere dei tentativi. </h1>";
+        echo "<a href=\"esercizio2.html\">Torna al form</a>";
+        $contatoretentativi++;
+
+    }
+    else if("$contatoretentativi > $tentativimassimi")
     {
 
         echo "<h1 style='color:black; text-align:center;'> Il tuo accesso è negato. Password e/o utente sono errati! Attenzione hai tre tentativi. </h1>";
-        
+
     }
             
-        ?>
-    </tbody>
-</table>
+    ?>
 
 </body>
 </html>
